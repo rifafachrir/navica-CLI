4250ac0f61ac5a3a53bfacea81ad375e9ae5a6ae
rifafachrir <rifafachriramadhan1806@gmail.com>
2025-12-03 21:46:23 +0700

    update authentication

--- authentication.py
+++ authentication.py
@@ -15,11 +15,26 @@ def authentication (username, password):
                 else:
                     print("Password salah!")
                     return False
+            else:
+                print("Username tidak ditemukan!")
+                return False
+
+def login():
+    looping = True
+    while looping:
+        username = input("Masukkan username: ")
+        password = input("Masukkan password: ")
+        if authentication(username, password):
+            looping = False
+            return True
 
 def register(username, password):
     user.append({'username': username, 'password': password, 'status': False})
     print("Registrasi berhasil!")
 
+def list():
+    for i in user:
+        print(i)
 
 def logout():
     print("Logout berhasil!")
@@ -31,17 +46,18 @@ def logout():
 
 def start_authentication():
     while True:
-        choice = input("Pilih opsi: 1. Login 2. Register 3. Logout\n")
+        choice = input("Pilih opsi: 1. Login 2. Register 3. List  0. Logout\n")
         if choice == '1':
-            username = input("Masukkan username: ")
-            password = input("Masukkan password: ")
-            if authentication(username, password):
-                return True
+            login()
         elif choice == '2':
             username = input("Masukkan username baru: ")
             password = input("Masukkan password baru: ")
             register(username, password)
         elif choice == '3':
+            list()
+        elif choice == '0':
            logout()
         else:
             print("Opsi tidak valid. Silakan coba lagi.")
+
+start_authentication()
